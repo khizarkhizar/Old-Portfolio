2d10bcd46ae376d4e4ba169451f52e689747c423
khizarkhizar <khizarrana756@gmail.com>
2021-01-20 16:58:19 +0500

    Updates

---
+++ precache-manifest.a84bf2d4095f329d996820f23ca79da5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2bf383f78554c53e6fcdb66a72acbd",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "3ca7a8cc3a98c8bac62c",
+    "url": "/web/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "29d57cd4062e11e5a77c",
+    "url": "/web/static/css/main.63a3e8a5.chunk.css"
+  },
+  {
+    "revision": "3ca7a8cc3a98c8bac62c",
+    "url": "/web/static/js/2.9eda30c4.chunk.js"
+  },
+  {
+    "revision": "2d86a7af75862c18c6abcb89735b1e5c",
+    "url": "/web/static/js/2.9eda30c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d57cd4062e11e5a77c",
+    "url": "/web/static/js/main.56e63e50.chunk.js"
+  },
+  {
+    "revision": "d7da8307f5ebdbec40fd",
+    "url": "/web/static/js/runtime-main.584260b9.js"
+  }
+]);
